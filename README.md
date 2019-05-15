44b0e46149d11f282edcb89c8c5426a3c4a255b7
hoisharka <hoisharka@gmail.com>
2019-05-15 09:48:25 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ice-tools-atom package
+
+기본문제/전과 제작 개선용 아톰 플러그인
---
+++ spec/ice-tools-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IceToolsAtomView from '../lib/ice-tools-atom-view';
+
+describe('IceToolsAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
